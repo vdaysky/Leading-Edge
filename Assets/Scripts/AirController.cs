b80177fa8c58624dedf974d1b1eddf6236021ed9
Vladimir Odaysky <54320658+vdaysky@users.noreply.github.com>
2023-02-26 19:34:43 +0200

    Plane Physics (#1)
    * Improve plane physics
    
    * Add used assets
    
    * Fix ground textures
    
    * Allow movement on roll axis

--- Assets/Scripts/AirController.cs
+++ Assets/Scripts/AirController.cs
@@ -1,66 +1,152 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Globalization;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
 
 public class AirController : MonoBehaviour
 {
-    [SerializeField] Rigidbody _planeRigidBody;
-    [SerializeField] AnimationCurve aoaCoefficient;
-    
-    private Vector3 ThrustVector;
-    private Vector3 GravityVector = Vector3.down * 10;
-    private Vector3 LiftVector;
-
-    private float angleOfAttack;
-
-    private Vector3 LocalVelocity;
-    private Vector3 LocalAngularVelocity;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
+    private enum CameraView {
+        Back,
+        Front
     }
+    
+    [SerializeField] private Rigidbody planeRigidBody;
+    
+    [SerializeField] private Camera mainCamera;
+    
+    [SerializeField] private ParticleSystem explosion;
+    
+    [SerializeField] private TextMeshProUGUI speedText;
+    
+    [SerializeField] private TextMeshProUGUI torqueText;
+    
+    [SerializeField] private TextMeshProUGUI rotationText;
 
-    // Update is called once per frame
-    void Update()
+    private CameraView _cameraView = CameraView.Back;
+    
+    private const float MaxSpeed = 52f;
+    private const float WingRollRate = 8;
+    private const float SteeringVSens = 26;
+    private const float SteeringHSens = 52;
+    private const float TorqueYawStartRollingBack = 0.05f;
+    private const float MaxPlaneTurningRoll = 35;
+
+    private void Update()
     {
+        Vector3 thrustVector;
+        var gravityVector = Vector3.down * 10;
+        
+        // lets say that when perfectly flat plane can counter gravity
+        var inverseGravityVector = Vector3.up * 10; 
+
         //Thrust change mostly for debug
-        if(Input.GetButton("Jump"))
+        if (Input.GetButton("Jump"))
         {
-            ThrustVector = _planeRigidBody.transform.forward * 10f;
+            thrustVector = planeRigidBody.transform.forward * 10f;
         }
         else
         {
-            ThrustVector = _planeRigidBody.transform.forward * 30f;
+            thrustVector = planeRigidBody.transform.forward * MaxSpeed;
         }
+        
+        // change camera view
+        if (Input.GetKeyDown(KeyCode.C)) {
+            _cameraView = _cameraView == CameraView.Back ? CameraView.Front : CameraView.Back;
+        }
+        
+        // project plane vertical vector on world vertical vector
+        Vector3 planeVerticalVector = planeRigidBody.transform.up;
+        Vector3 worldVerticalVector = Vector3.up;
+        Vector3 planeVerticalVectorProjected = Vector3.Project(planeVerticalVector, worldVerticalVector);
+        float verticalSpeedCut = planeVerticalVectorProjected.magnitude;
+        
+        // compute lift vector (consider lift force to be proportional to vertical speed) 
+        Vector3 liftVector = inverseGravityVector * verticalSpeedCut;
+        
+        Vector3 finalVelocity = thrustVector + liftVector + gravityVector;
+        planeRigidBody.velocity = finalVelocity;
 
-        float aoaLift = aoaCoefficient.Evaluate(angleOfAttack * Mathf.Rad2Deg);
-        LiftVector = _planeRigidBody.transform.up * (_planeRigidBody.velocity.sqrMagnitude * aoaLift * 0.01f);
-
-        Quaternion invRotation = Quaternion.Inverse(_planeRigidBody.rotation);
-        LocalVelocity = invRotation * _planeRigidBody.velocity;
-        LocalAngularVelocity = invRotation * _planeRigidBody.angularVelocity;
-
-        _planeRigidBody.velocity = ThrustVector + GravityVector + LiftVector;
+        speedText.text = $"Speed: {planeRigidBody.velocity.magnitude}\n" +
+                         $"{planeRigidBody.velocity}\nLift: {verticalSpeedCut}";
     }  
 
     //FixedUpdate is called zero, one or multipe times per frame
     private void FixedUpdate()
     {
-        angleOfAttack = Mathf.Atan2(-LocalVelocity.y, LocalVelocity.z);
         PlaneSteering();
+        CameraFollow();
     }
 
-
-
-    void PlaneSteering()
+    private void OnCollisionEnter(Collision collision)
     {
-        float Vert = Input.GetAxisRaw("Vertical") * 30f * Time.deltaTime;
-        float Roll = Input.GetAxisRaw("Roll") * 60f * Time.deltaTime;
-        float Hor = Input.GetAxisRaw("Horizontal") * 30f * Time.deltaTime;
-        Vector3 TargetTorque = new Vector3(Vert - LocalAngularVelocity.x, Hor - LocalAngularVelocity.y, Roll - LocalAngularVelocity.z);
+        var other = collision.gameObject;
+        
+        if (other.CompareTag("WantedTarget"))
+        {
+            // TODO:
+            // Win, hook up to something here
+        }
+        else
+        {
+            // TODO:
+            // Lost, hook up to something here
+        }
 
-        _planeRigidBody.AddRelativeTorque(TargetTorque, ForceMode.VelocityChange);
+        Instantiate(explosion, planeRigidBody.transform.position, Quaternion.identity);
+        Destroy(planeRigidBody.gameObject, 0);
+        
+        
+    }
+
+    private void CameraFollow()
+    {
+        // set camera position
+        var planeTransform = planeRigidBody.transform;
+        var planeRotation = planeTransform.rotation;
+        
+        var distance = 30;
+        const int height = 5;
+        
+        var view = Quaternion.Euler(0, 0, 0);
+        if (_cameraView == CameraView.Front)
+        {
+            distance = -distance;
+            view = Quaternion.Euler(0, 180, 0);
+        }
+
+        mainCamera.transform.position = planeTransform.position - planeTransform.forward * distance + planeTransform.up * height;
+        
+        // set camera rotation slowly over time to follow plane, but ignore roll
+        Quaternion planeRotationFixed = Quaternion.Euler(
+            planeRotation.eulerAngles.x, 
+            planeRotation.eulerAngles.y, 
+            0
+        );
+        planeRotationFixed *= view;
+        mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, planeRotationFixed, 0.125f);
+    }
+
+    private void PlaneSteering()
+    {
+        rotationText.text = $"Rotation:\nUp: {Math.Round(planeRigidBody.rotation.eulerAngles.x, 2)}\n" +
+                            $"Left: {Math.Round(planeRigidBody.rotation.eulerAngles.y, 2)}\n" +
+                            $"Roll: {Math.Round(planeRigidBody.rotation.eulerAngles.z, 2)}";
+        
+        float vert = Input.GetAxisRaw("Vertical") * SteeringVSens;
+        float hor = Input.GetAxisRaw("Horizontal") * SteeringHSens;
+        float roll = Input.GetAxisRaw("Roll") * SteeringHSens;
+
+        Vector3 targetTorque = new Vector3(-vert, hor, roll);
+        planeRigidBody.AddRelativeTorque(targetTorque, ForceMode.Force);
+
+        // get plane torque in local space
+        Vector3 torque = planeRigidBody.transform.InverseTransformDirection(planeRigidBody.angularVelocity);
+        torqueText.text = "Torque:\n" +
+                          $"Up: {Math.Round(torque.x, 2)}\n" +
+                          $"Left: {Math.Round(torque.y, 2)}\n" +
+                          $"Roll: {Math.Round(torque.z, 2)}";
     }
 }
